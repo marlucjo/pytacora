df7c3ac8c194f2601176e1787c674dee7e33ca59
marlucjo <marlucjo@gmail.com>
2023-11-11 20:38:47 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'pytacora'
+copyright = '2023, marlucjo'
+author = 'marlucjo'
 
 release = '0.1'
 version = '0.1.0'
